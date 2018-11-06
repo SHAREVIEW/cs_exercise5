d2be278f8bee1f1d7d65cf150103033eec025cb3
david <2657070472@qq.com>
2018-11-06 17:11:28 +0800

    do exercise

--- cs_exercise5/Program.cs
+++ cs_exercise5/Program.cs
@@ -9,7 +9,18 @@ namespace cs_exercise5
     class Program
     {
         static void Main(string[] args)
+
         {
+            //接受用户输入的字符串，将其中的字符与输入相反的顺序输出
+            Console.WriteLine("Enter a string:");
+            string myString = Console.ReadLine();
+            string reversedString = "";
+            for (int index = myString.Length - 1; index >= 0; index--)
+            {
+                reversedString += myString[index];
+            }
+            Console.WriteLine("Reversed: {0}", reversedString);
+            Console.ReadKey();
         }
     }
 }
